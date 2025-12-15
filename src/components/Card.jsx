2a10213a14909579656a90200b9b07d9cc91ad43
Danloveper <danloveper@gmail.com>
2025-12-14 19:51:53 -0500

    Test

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,6 @@
 
 const Card = ({ id, name, email}) => {
 
-
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
         <img src={`https://robohash.org/${id}?200x200`} alt="robofriend" />
